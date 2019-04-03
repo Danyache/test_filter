7f6ddbcc21b340c2660d13ba89e4a0735549a4eb
danyache1812 <danyache1812@yandex.ru>
2019-04-03 20:53:19 +0300

    first version

--- test_filter_bot.py
+++ test_filter_bot.py
@@ -9,13 +9,83 @@ import re
 
 TOKEN = '889443907:AAHUP8IQRoNQ4fWXuIpd5mML-fBwbRaANuM'
 
+prices = {}
+brands = {}
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+def get_top_items(brand=None, price=None):
+    url = "https://www.mvideo.ru/noutbuki-planshety-komputery/noutbuki-118/f/"
+    
+    if brand:
+        brands = str(brand[0])
+        for b in brand[1:]:
+            brands = brands + ',' + str(b)
+        url = url + 'brand=' + brands + '/'
+    
+    if price:
+        url = url + 'price=from-' + price[0] + '-to-' + price[1]
+        
+    r = requests.get(url, headers={'User-agent':generate_user_agent()})
+    
+    soup = BeautifulSoup(r.text, "lxml")
+    
+    img = soup.find('div', class_="c-product-tile").find('img')['data-original'][2:]
+    item_price = soup.find('div', class_="c-product-tile").find('div', class_='c-pdp-price__current').text.replace('\xa0', '').replace('\t', '').replace('\n', '').replace(' ', '')
+    href = soup.find('div', class_="c-product-tile").find('a')['href']
+    href = "https://www.mvideo.ru" + href
+    return img, href, item_price
+
+
+@dp.message_handler(commands=['start'], commands_prefix='!/')
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nДавай выберем тебе ноутбук!")
+
+@dp.message_handler(commands=['help'], commands_prefix='!/')
+async def process_help_command(message: types.Message):
+    await bot.send_message(message.from_user.id, "Давай выберем тебе ноутбук \n \
+    	/price -- напиши минимальную и максимальную цены через пробел \n \
+    	/brand -- напиши через пробел производителей из данного списка: Apple, Acer, Asus, HP, Lenovo \n \
+    	/items -- по данной команде бот выдаст тебе товары")
+
+@dp.message_handler(commands=['price'], commands_prefix='!/')
+async def process_price_command(message: types.Message):
+	global prices
+	prices[message.chat.id] = message.text.split()
+
+@dp.message_handler(commands=['brand'], commands_prefix='!/')
+async def process_brand_command(message: types.Message):
+	global brands
+	brands[meassage.chat.id] = message.text.split()
+
+@dp.message_handler(commands=['items'], commands_prefix='!/')
+async def process_items_command(message: types.Message):
+	global brands
+	global prices
+	chat_brands = None
+	chat_prices = None
+	try:
+		chat_brands = brands[message.chat.id]
+	except BaseException:
+		pass
+	try:
+		chat_prices = prices[message.chat.id]
+	except BaseException:
+		pass
+
+	img_url, href, item_price = get_top_items(chat_brands, chat_prices)
+	tell_price = 'Цена на данный ноутбук составляет ' + item_price
+	await bot.send_photo(message.chat.id, types.InputFile.from_url(img_url))
+	await bot.send_message(message.from_user.id, tell_price)
+	await bot.send_message(message.from_user.id, href)
+
+
+
+
 @dp.message_handler()
 async def message_handler(msg: types.Message):
-	info = 'Съешь еще этих мягких французских булок, да выпей чаю'
+	info = 'Я не чат-бот, извини. Задай, пожалуйста, параметры ноутбука.'
 	await bot.send_message(msg.from_user.id, info)
 
 if __name__ == '__main__':
