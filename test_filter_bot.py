efacab1951655993a88218af9c5c025763207ef3
danyache1812 <danyache1812@yandex.ru>
2019-04-03 21:09:01 +0300

    FINALLY?

--- test_filter_bot.py
+++ test_filter_bot.py
@@ -27,7 +27,7 @@ def get_top_items(brand=None, price=None):
     if price:
         url = url + 'price=from-' + price[0] + '-to-' + price[1]
         
-    r = requests.get(url, headers={'User-agent':generate_user_agent()})
+    r = requests.get(url) #, headers={'User-agent':generate_user_agent()})
     
     soup = BeautifulSoup(r.text, "lxml")
     
